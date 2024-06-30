f6a17cf1ed5271c1990afaa5da34da94b5a871cb
birdfromhell <95958484+birdfromhell@users.noreply.github.com>
2024-06-30 19:32:40 +0000

    Update app.py, templates, and static files

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request, url_for, redirect, flash
+from flask import Flask, render_template, request, url_for, redirect, flash, session
 from flask_mysqldb import MySQL
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from dotenv import load_dotenv
 import os
+from datetime import timedelta
 
 load_dotenv()
 
@@ -11,23 +13,65 @@ app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
 app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=1)
 
 mysql = MySQL(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
 app.secret_key='lloveyou'
     
-@app.route('/')
-def index():
+
+class User(UserMixin):
+    pass
+
+@login_manager.user_loader
+def user_loader(user_id):
     cur = mysql.connection.cursor()
-    cur_client = mysql.connection.cursor()
-    cur.execute("""SELECT tb_m_project.*, tb_m_client.client_name, DATE_FORMAT(project_start, "%d %b %Y") AS formatted_start, DATE_FORMAT(project_end, "%d %b %Y") AS formatted_end 
-FROM tb_m_project INNER JOIN tb_m_client ON tb_m_project.client_id = tb_m_client.client_id;""")
-    cur_client.execute('SELECT * FROM tb_m_client')
-    data = cur.fetchall()
-    data_client = cur_client.fetchall()
+    cur.execute("SELECT * FROM users WHERE id = %s", (user_id,))
+    user = cur.fetchone()
     cur.close()
-    cur_client.close()
-    return render_template('index.html', projects=data, clients=data_client)
+    if user:
+        user_obj = User()
+        user_obj.id = user[0]
+        return user_obj
+    return None
+    
+
+@app.route('/')
+@login_required
+def index():
+        cur = mysql.connection.cursor()
+        cur_client = mysql.connection.cursor()
+        cur.execute("""SELECT tb_m_project.*, tb_m_client.client_name, DATE_FORMAT(project_start, "%d %b %Y") AS formatted_start, DATE_FORMAT(project_end, "%d %b %Y") AS formatted_end 
+        FROM tb_m_project INNER JOIN tb_m_client ON tb_m_project.client_id = tb_m_client.client_id;""")
+        cur_client.execute('SELECT * FROM tb_m_client')
+        data = cur.fetchall()
+        data_client = cur_client.fetchall()
+        cur.close()
+        cur_client.close()
+        return render_template('index.html', projects=data, clients=data_client)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM users WHERE username = %s AND password = %s", (username, password))
+        user = cur.fetchone()
+        cur.close()
+        if user:
+            user_obj = User()
+            user_obj.id = user[0]
+            session.permanent = True
+            login_user(user_obj)
+            return redirect(url_for('index'))
+        else:
+            return 'Invalid username or password'
+    return render_template('login.html')
 
 @app.route('/add_project', methods=['POST'])
 def add_project():
@@ -89,4 +133,3 @@ def change_status(project_id):
 
 if __name__=='__main__':
     app.run(port=8080, debug=True)
-
